917938b5b61ece7cc11e15324babc48616a785f2
wimba juventio <wimbajuventio@yahoo.com>
2019-05-20 12:16:12 +0700

    Initial Commit

---
+++ Nano Challange 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nano Challange 1
+//
+//  Created by Wimba Juventio Chandra on 20/05/19.
+//  Copyright © 2019 Wimba Juventio Chandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
