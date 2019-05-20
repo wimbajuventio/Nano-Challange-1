14fad35434a8d5ce91bbe07ad1544d70cb611315
wimba juventio <wimbajuventio@yahoo.com>
2019-05-20 14:10:08 +0700

    shape moving

--- Nano Challange 1/ViewController.swift
+++ Nano Challange 1/ViewController.swift
@@ -18,13 +18,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         envyButton.layer.cornerRadius = envyButton.frame.width/2
         blessedButton.layer.cornerRadius = blessedButton.frame.width/2
+        
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func envyButtonClicked(_ sender: Any) {
-        gedeinButton()
+        gedeinButtonEnvy()
     }
-    func gedeinButton ()
+    func gedeinButtonEnvy ()
     {
         UIView.animate(withDuration: 1)
         {
@@ -64,47 +66,59 @@ class ViewController: UIViewController {
             }
             print(self.clickedAnimation)
             self.clickedAnimation += 1
+                 self.view.sendSubviewToBack(self.envyButton)
+                self.view.bringSubviewToFront(self.blessedButton)
         }
     }
+    
+    @IBAction func blessedButtonClicked(_ sender: Any) {
+        gedeinButtonBlessed()
+    }
+    
+    func gedeinButtonBlessed ()
+    {
+    
     UIView.animate(withDuration: 1)
     {
     switch self.clickedAnimationBlessed
     {
-    case 0:
-    self.envyButton.transform = CGAffineTransform (scaleX: 1.5, y: 1.5)
-    
-    case 1:
-    self.envyButton.transform = CGAffineTransform (scaleX: 2, y: 2)
-    case 2:
-    self.envyButton.transform = CGAffineTransform (scaleX: 2.5, y: 2.5)
-    case 3:
-    self.envyButton.transform = CGAffineTransform (scaleX: 3, y: 3)
-    case 4:
-    self.envyButton.transform = CGAffineTransform (scaleX: 3.5, y: 3.5)
-    case 5:
-    self.envyButton.transform = CGAffineTransform (scaleX: 4, y: 4)
-    case 6:
-    self.envyButton.transform = CGAffineTransform (scaleX: 4.5, y: 4.5)
-    case 7:
-    self.envyButton.transform = CGAffineTransform (scaleX: 5, y: 5)
-    case 8:
-    self.envyButton.transform = CGAffineTransform (scaleX: 5.5, y: 5.5)
-    case 9:
-    self.envyButton.transform = CGAffineTransform (scaleX: 6, y: 6)
-    case 10:
-    self.envyButton.transform = CGAffineTransform (scaleX: 6.5, y: 6.5)
-    case 11:
-    self.envyButton.transform = CGAffineTransform (scaleX: 7, y: 7)
-    case 12:
-    self.envyButton.transform = CGAffineTransform (scaleX: 7.5, y: 7.5)
-    case 13:
-    self.envyButton.transform = CGAffineTransform (scaleX: 8, y: 8)
+        case 0:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 1.5, y: 1.5)
+        case 1:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 2, y: 2)
+        case 2:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 2.5, y: 2.5)
+        case 3:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 3, y: 3)
+        case 4:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 3.5, y: 3.5)
+        case 5:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 4, y: 4)
+        case 6:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 4.5, y: 4.5)
+        case 7:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 5, y: 5)
+        case 8:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 5.5, y: 5.5)
+        case 9:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 6, y: 6)
+        case 10:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 6.5, y: 6.5)
+        case 11:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 7, y: 7)
+        case 12:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 7.5, y: 7.5)
+        case 13:
+          self.blessedButton.transform = CGAffineTransform (scaleX: 8, y: 8)
     default:
     break
     }
-    print(self.clickedAnimation)
-    self.clickedAnimation += 1
+    print(self.clickedAnimationBlessed)
+    self.clickedAnimationBlessed += 1
+        self.view.sendSubviewToBack(self.blessedButton)
+        self.view.bringSubviewToFront(self.envyButton)
+    }
+        }
     }
 
-}
 
